f1567009b3d7c73f36ee7ccd0e4873ccb3773224
fishmonger45 <fishmonger45@proton.me>
2022-11-11 12:21:15 +1300

    structure print

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod parser;
 fn main() {
     let wat = fs::read_to_string("add.wat").unwrap();
     let (_, ast) = module_entire(&wat).expect("Couldn't parse wat");
-    println!("{:?}", ast);
+    println!("{:#?}", ast);
     let bytes = compile::compile(&ast);
     let mut fh = File::create("add.wasm").expect("Failed to create wasm");
     fh.write_all(&bytes).expect("Failed to write wasm");
